83a1eefc1e3104377f007486b8b2909320140a17
Jacob Nyyssønen Henriksen <jaco9515@zbc.dk>
2022-11-28 13:58:06 +0100

    Working on getting push not... to work

--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -1,4 +1,7 @@
+export 'dart:io';
 export 'package:flutter/material.dart';
+export 'package:flutter_local_notifications/flutter_local_notifications.dart';
 export 'package:firebase_core/firebase_core.dart';
-export 'package:flutter/material.dart';
+export 'package:firebase_messaging/firebase_messaging.dart';
+export 'package:device_info_plus/device_info_plus.dart';
 export 'nav.dart';
